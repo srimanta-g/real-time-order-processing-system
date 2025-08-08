2552c0a7c67d2469c261ddd04adf7fcc6bed159d
srimanta-g <srimantaganguly02@gmail.com>
2025-08-09 01:43:43 +0530

    add test for product service - to test if the queuing mechanism is working properly

--- productservice/src/test/java/com/srimantatech/productservice/ProductserviceApplicationTests.java
+++ productservice/src/test/java/com/srimantatech/productservice/ProductserviceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.srimantatech.productservice;
 
+import com.srimantatech.productservice.dto.OrderProductDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.EnableTestBinder;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.in;
 
 @SpringBootTest
+@EnableTestBinder
 class ProductserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private InputDestination input;
+	@Autowired
+	private OutputDestination output;
 
+	@Test
+	public void testProductAvailability() {
+		input.send(MessageBuilder.withPayload(new OrderProductDto(1, 1, 1)).build(),
+				"product-availability-queue");
+		Message<byte[]> message = output.receive(10000, "product-confirmation-queue");
+
+		assertThat(Boolean.parseBoolean(new String(message.getPayload())))
+				.isTrue();
+	}
 }
